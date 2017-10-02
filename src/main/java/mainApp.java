63cbf92923c92c6569d1a5da3082a531beb52067
eldadyaakobi <eldad.yaakobi@gmail.com>
2017-10-02 23:21:31 +0300

    remove mainApp.java

--- src/main/java/mainApp.java
+++
@@ -1,9 +0,0 @@
-import graphicInterface.MainFrame;
-
-public class mainApp
-{
-    public static void main(String[] args) {
-        MainFrame mainFrame = new MainFrame();
-        mainFrame.createMainFrame();
-    }
-}
